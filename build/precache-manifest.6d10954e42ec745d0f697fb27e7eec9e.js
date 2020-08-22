d8021bfe3db364d70425c0ea87bc032082894006
Vivek Kumar <vivekkumar27june88@gmail.com>
2020-08-22 23:31:42 +0530

    build for github pages

---
+++ build/precache-manifest.6d10954e42ec745d0f697fb27e7eec9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9526e1952837fe9ddbdb04c46e59f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4433b8ca1b0a0c86c88",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "9f7cb2d0a10d1c762466",
+    "url": "/static/css/main.0652593d.chunk.css"
+  },
+  {
+    "revision": "b4433b8ca1b0a0c86c88",
+    "url": "/static/js/2.74f59bfd.chunk.js"
+  },
+  {
+    "revision": "b7189180dbce6d61f34aa971007229e0",
+    "url": "/static/js/2.74f59bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7cb2d0a10d1c762466",
+    "url": "/static/js/main.f6c139df.chunk.js"
+  },
+  {
+    "revision": "62a7e0a19ea07734d5ef",
+    "url": "/static/js/runtime-main.08cdaebf.js"
+  }
+]);
